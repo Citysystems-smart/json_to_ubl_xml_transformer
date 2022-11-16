ad12b12876776b9bcb764467f92182aeb2a4c19f
ali.alshaqaq <ali.alshaqaq@smart.bhh>
2022-11-16 11:58:56 +0000

    6

--- json_to_ubl_xml_transformer/constants.py
+++ json_to_ubl_xml_transformer/constants.py
@@ -17,7 +17,7 @@ TEXT_CONTENT = "#text"
 BILLING_NAMESPACE = (
     "urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0"
 )
-PROFILE_NAMESPACE = "urn:fdc:peppol.eu:2017:poacc:billing:01:1.0"
+PROFILE_NAMESPACE = "reporting:1.0"
 INVOICE_NAMESPACE = "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2"
 CAC_NAMESPACE = (
     "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
